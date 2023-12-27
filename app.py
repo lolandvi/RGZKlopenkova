168c33c5144015afa9459cafc502282e9cbc1a2c
Клопенкова Виктория <lvikulka@bk.ru>
2023-12-27 07:12:12 +0700

    Корректная реализация входа

--- app.py
+++ app.py
@@ -1,40 +1,61 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_jsonrpc import JSONRPC
-from flask import redirect, url_for
+from flask import Blueprint, Flask, render_template, redirect, request, url_for, jsonify
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///api_database.db'
-db = SQLAlchemy(app)
-jsonrpc = JSONRPC(app)
+bcrypt = Bcrypt(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-with app.app_context():
-    db.create_all()
-
-@jsonrpc.method("user.get_user_by_id", authenticated=False)
-def get_user_by_id(user_id: int) -> dict:
-    user = User.query.get(user_id)
-    if user:
-        return {'username': user.username, 'id': user.id}
-    else:
-        return {'error': 'User not found'}
+# Простые списки для хранения имен пользователей и их хешированных паролей
+usernames = []
+password_hashes = []
 
 @app.route("/mainpage")
 def mainpage():
     return render_template('mainpage.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Обработка данных формы входа
+        username = request.form['username']
+        password = request.form['password']
+
+        # Проверка логина и пароля пользователя
+        if username in usernames and bcrypt.check_password_hash(password_hashes[usernames.index(username)], password):
+            return redirect(url_for('mainpage'))
+        else:
+            error_message = "Аккаунт не найден. Перепроверьте корректность данных или создайте аккаунт"
+            return render_template('login.html', error_message=error_message)
+
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        # Обработка данных формы регистрации
+        username = request.form['username']
+        password = request.form['password']
+
+        # Хеширование пароля перед сохранением
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Добавление пользователя в списки
+        usernames.append(username)
+        password_hashes.append(hashed_password)
+
+        return redirect(url_for('login'))  # Перенаправление на страницу входа после успешной регистрации
+
     return render_template('register.html')
+
+@app.route('/check_login', methods=['POST'])
+def check_login():
+    if request.method == 'POST':
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        if username in usernames and bcrypt.check_password_hash(password_hashes[usernames.index(username)], password):
+            return jsonify({'success': True})
+        else:
+            return jsonify({'success': False, 'message': 'Аккаунт не найден. Перепроверьте корректность данных или создайте аккаунт'})
 
 if __name__ == '__main__':
     app.run(debug=True)
